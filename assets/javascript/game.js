fdecdab7e9a6c1d7bda86f6d85c676fb0d1b1757
dani <gehr.danielle@gmail.com>
2020-03-07 12:41:33 -0500

    added an image button

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+console.log("hello")
+
+var scooby
+var shaggy
+var velma
+var daphne
+var fred
+var knight
+var phantom
+var cutler
+var miner
+var witchDoctor
+var wins
+var losses
+var ties
+
+var onclick = function () {
+    // when user clicks scooby doo his picture will go to your mystery gang member and the other members will stay under choose your mystery gang member
+    // when user clicks shaggy his picture will go to your mystery gang member and the other members will stay under choose your mystery gang member
+    // when user clicks velma her picture will go to your mystery gang member and the other members will stay under choose your mystery gang member
+    // when user clicks daphne her picture will go to your mystery gang member and the other members will stay under choose your mystery gang member
+    // when the user clicks fred his picture will go to your mystery gang member and the other members will stay under choose your mystery gang member
+}
+
+var onclick = function () {
+    // when user clicks knight his picture will go to Villian and the other members will stay under choose your villian
+    // when user clicks shaggy his picture will go to your mystery gang member and the other members will stay under choose your villian
+    // when user clicks velma her picture will go to your mystery gang member and the other members will stay under choose your villian
+    // when user clicks daphne her picture will go to your mystery gang member and the other members will stay under choose  your villian
+    // when the user clicks fred his picture will go to your mystery gang member and the other members will stay under choose your villian
+}
+
+var onclick = function () {
+    var options =[min="1", max="40"]
+    // when the user hits solve the computer will randomly generate 2 numbers between 1 - 40
+    // the computer will then compare those two numbers 
+    // the computer will then pick the highest number
+    // the character with the highest number wins and will keep their solve/scare points
+    // the character that loses their solve/scare points will decrease
+    // if their is a tie neither character loses health points
+}
+var randomNumber = 100
+var computerGuess = randomNumber[Math.floor(Math.random() * 100)]
